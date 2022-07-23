4769b25ed6a9084ed10b3c2db914523fa9adce7e
mikael <mikaelhirpo@gmail.com>
2022-07-23 19:23:14 +0300

    testing the features

--- App.tsx
+++ App.tsx
@@ -22,6 +22,8 @@ import {
   Box,
 } from 'native-base';
 import NativeBaseIcon from './src/components/NativeBaseIcon';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 // Color Switch Component
 function ToggleDarkMode() {
@@ -50,24 +52,13 @@ const App = () => {
         flex={1}>
         <VStack space={5} alignItems="center">
           <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
+          <Heading size="lg">Ye begena mezmurat</Heading>
+          
+        
             <Text color="primary.500" underline fontSize={'xl'}>
-              Learn NativeBase
+              List goes here
             </Text>
-          </Link>
+         
           <ToggleDarkMode />
         </VStack>
       </Center>
